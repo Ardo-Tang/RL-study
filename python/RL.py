0df4bbaf129c34766d19ad8f5fb6075a0ca591e9
Ardo-Tang <z7193028@gmail.com>
2020-02-19 18:30:43 +0800

    RL

--- python/RL.py
+++ python/RL.py
@@ -1,6 +1,9 @@
 import gym
 from gym import envs
 import numpy as np
+from math import log2
+from keras import Sequential
+from keras.layers import Convolution2D, MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization, LeakyReLU
 
 class RL(object):
 
@@ -8,13 +11,51 @@ class RL(object):
         self.env = gym.make(env_name)
         self.action_space = self.env.action_space
         self.learning_rate = learning_rate
+        self.policy = self.mkmodel()
         
     def step(self, action):
         state, reward, game_over, _ = self.env.step(action)
-        # observation：環境狀態，像是影像 pixel，角度，角速度等，參數意義必須參照原始碼
+        # state：環境狀態，像是影像 pixel，角度，角速度等，參數意義必須參照原始碼
         # reward：越好的結果，會回饋越大的值，若不適用，也可從 observation 自行產生
-        # done：判斷是否達到 game over 的條件，像是生命已結束，或是已經超出範圍
+        # game_over：判斷是否達到 game over 的條件，像是生命已結束，或是已經超出範圍
         # info：debug 用的資訊，不允許使用在學習上
         return state, reward, game_over
+
+    def mkmodel(self):
+        model = Sequential()
+        model.add(Convolution2D(filters=32, kernel_size=3, input_shape=self.env.observation_space.shape))
+        model.add(LeakyReLU())
+        model.add(Convolution2D(filters=32, kernel_size=3))
+        model.add(LeakyReLU())
+        model.add(MaxPooling2D(pool_size=2, padding="same"))
+
+        model.add(Convolution2D(filters=64, kernel_size=3))
+        model.add(LeakyReLU())
+        model.add(Convolution2D(filters=64, kernel_size=3))
+        model.add(LeakyReLU())
+        model.add(MaxPooling2D(pool_size=2, padding="same"))
+
+        model.add(Flatten())
+        
+        output_shape = 2 ** int(log2(model.output_shape[1]))
+        while(int(output_shape/2)>=256):
+            model.add(Dense(int(output_shape/2)))
+            model.add(LeakyReLU())
+            model.add(BatchNormalization())
+            model.add(Dropout(0.5))
+            output_shape = int(output_shape/2)
+
+        model.add(Dense(self.action_space.n))
+        
+        model.summary()
+        return model
+
+    def train(self):
+        self.policy.compile(optimizer="Nadam", loss="mse")
+
+        
+
+if __name__ == "__main__":
+    RL = RL()
     
-    
+    pass
--- python/gymEnv.py
+++ python/gymEnv.py
@@ -1,5 +1,6 @@
 import gym
-from gym import envs
+# from gym import envs
+import numpy as np
  
 # 列出可用的遊戲
 # print(envs.registry.all())
@@ -16,8 +17,11 @@ print(env.unwrapped.get_action_meanings())
 # 查看環境中可用的 observation 有多少個
 print(env.observation_space)    
 '''
+print(env.action_space.n)
+print(env.unwrapped.get_action_meanings())
+# print(np.shape(env.observation_space.high))
 # 查看 observation 最高值
-print(env.observation_space.high)   
+# print(env.observation_space.high)   
 # 查看 observation 最低值
 # print(env.observation_space.low)    
  
@@ -32,13 +36,13 @@ for i_episode in range(20):
         action = env.action_space.sample()
         # 輸入動作，並得到狀態結果
         observation, reward, done, info = env.step(action)
-        
+        print(reward)
         # observation：環境狀態，像是影像 pixel，角度，角速度等，參數意義必須參照原始碼
         # reward：越好的結果，會回饋越大的值，若不適用，也可從 observation 自行產生
         # done：判斷是否達到 game over 的條件，像是生命已結束，或是已經超出範圍
         # info：debug 用的資訊，不允許使用在學習上
         
-        print(observation)
+        # print(observation)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
